66c1184f04ef898f7cd4553b4beceaba653d987d
LangInteger <709091988@qq.com>
2020-02-21 00:58:55 +0800

    complete hw02 make adder lambda

---
+++ programming/SICP/Lecture4/make_adder_lambda.py
@@ -0,0 +1,11 @@
+def make_adder(n):
+    """Return a function that takes an argument K and returns N + K.
+
+    >>> add_three = make_adder(3)
+    >>> add_three(1) + add_three(2)
+    9
+    >>> make_adder(1)(2)
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    return lambda x: n + x
