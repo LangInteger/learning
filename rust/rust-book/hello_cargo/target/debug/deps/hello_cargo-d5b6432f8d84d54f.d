8c964a4e9196131f1df6f6fc091e34a9b7825746
LangInteger <709091988@qq.com>
2020-11-13 01:45:57 +0800

    feat - rust hello world

---
+++ rust/rust-book/hello_cargo/target/debug/deps/hello_cargo-d5b6432f8d84d54f.d
@@ -0,0 +1,5 @@
+/home/langinteger/gitrepo/langinteger/learning/rust/rust-book/hello_cargo/target/debug/deps/hello_cargo-d5b6432f8d84d54f: src/main.rs
+
+/home/langinteger/gitrepo/langinteger/learning/rust/rust-book/hello_cargo/target/debug/deps/hello_cargo-d5b6432f8d84d54f.d: src/main.rs
+
+src/main.rs:
---
+++ rust/rust-book/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/langinteger/gitrepo/langinteger/learning/rust/rust-book/hello_cargo/target/debug/hello_cargo: /home/langinteger/gitrepo/langinteger/learning/rust/rust-book/hello_cargo/src/main.rs
